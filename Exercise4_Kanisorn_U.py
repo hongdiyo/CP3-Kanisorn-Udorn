e328c38836a8766deaffa825006c8dadfc042ea4
Kanisorn <56026905+hongdiyo@users.noreply.github.com>
2019-10-06 01:24:46 +0700

    Add files via upload

---
+++ Exercise4_Kanisorn_U.py
@@ -0,0 +1,9 @@
+FE = 80.01
+GB = 80.02
+ICS = 80.03
+CP = 80.04
+print("---Your Score---")
+print("Foundation English : ",FE)
+print("General Business : ",GB)
+print("Introduction to Computer Systems : ",ICS)
+print("Computer Programming : ",CP)
