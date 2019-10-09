820b644baf0240c05eedad5c72ecbcb831a2be3d
Kanisorn <56026905+hongdiyo@users.noreply.github.com>
2019-10-09 16:06:33 +0700

    Add files via upload

---
+++ Exercise5_1_Kanisorn_U.py
@@ -0,0 +1,7 @@
+x = int(input("Enter Number"))
+y = int(input("Enter Number"))
+print("x+y=",x+y)
+print("x-y=",x-y)
+print("x*y=",x*y)
+print("x/y=",x/y)
+
---
+++ Exercise5_2_Kanisorn_U.py
@@ -0,0 +1,4 @@
+s = int(input("Enter Distance (km): "))
+t = int(input("Enter Time (h): "))
+v = s/t
+print(v,"km/h")
