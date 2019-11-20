26c3f0280ad3a9b4767a28fb026ab4194bcb7e6e
Kanisorn <56026905+hongdiyo@users.noreply.github.com>
2019-11-20 11:31:38 +0700

    Add files via upload

---
+++ FunctionParemExample.py
@@ -0,0 +1,12 @@
+def add1(x,y):
+    print(x+y)
+def add2(x,y):
+    print(x-y)
+def add3(x,y):
+    print(x*y)
+def add4(x,y):
+    print(x/y)
+add1(100,100)
+add2(100,100)
+add3(100,100)
+add4(100,100)
